6e5c396426838e15f026468732c8a2c383638507
wulu <wulu90@outlook.com>
2024-12-05 15:15:56 +0800

    use std is_sorted, add bubble sort

--- 05.cpp
+++ 05.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <charconv>
+#include <cstddef>
 #include <cstdio>
 #include <fstream>
 #include <map>
@@ -12,19 +13,28 @@
 
 using namespace std;
 
-bool check_order(const map<int, set<int>>& rules_r, const vector<int>& update) {
-    for (size_t i = 0; i < update.size() - 1; ++i) {
-        if (!rules_r.contains(update[i])) {
+bool check_order(const map<int, set<int>>& rules, const vector<int>& update) {
+    for (auto i = update.rbegin(); i != next(update.rend(), -1); ++i) {
+        if (!rules.contains(*i)) {
             continue;
         }
-        auto before_cur = rules_r.at(update[i]);
-        for (size_t j = i + 1; j < update.size(); ++j) {
-            if (before_cur.contains(update[j])) {
+        auto greater_curr = rules.at(*i);
+        for (auto j = next(i); j != update.rend(); ++j) {
+            if (greater_curr.contains(*j)) {
                 return false;
             }
         }
     }
     return true;
+}
+
+bool check_order_1(const map<int, set<int>>& rules, const vector<int>& update) {
+    return is_sorted(update.begin(), update.end(), [&rules](auto num1, auto num2) -> bool {
+        if (rules.contains(num1) && rules.at(num1).contains(num2)) {
+            return true;
+        }
+        return false;
+    });
 }
 
 void fix_order(const map<int, set<int>>& rules, vector<int>& update) {
@@ -36,11 +46,20 @@ void fix_order(const map<int, set<int>>& rules, vector<int>& update) {
     });
 }
 
+void fix_order_1(const map<int, set<int>>& rules, vector<int>& update) {
+    for (size_t i = update.size() - 1; i > 1; --i) {
+        for (size_t j = update.size() - 1; j > update.size() - 1 - i; --j) {
+            if (rules.contains(update[j]) && rules.at(update[j]).contains(update[j - 1])) {
+                swap(update[j], update[j - 1]);
+            }
+        }
+    }
+}
+
 void part1() {
     ifstream input("input/input-5");
 
     map<int, set<int>> rules;
-    map<int, set<int>> rules_r;
     vector<vector<int>> updatevec;
     for (string line; getline(input, line);) {
         if (line.contains('|')) {
@@ -50,7 +69,6 @@ void part1() {
             int num2 = 0;
             from_chars(line.data() + sep + 1, line.data() + line.size(), num2);
             rules[num1].insert(num2);
-            rules_r[num2].insert(num1);
         } else if (line.contains(',')) {
             auto update = line | ranges::views::split(',') | views::transform([](auto&& subr) -> int {
                               string_view sv{subr.begin(), subr.end()};
@@ -66,11 +84,12 @@ void part1() {
     int res  = 0;
     int res2 = 0;
     for (auto& update : updatevec) {
-        if (check_order(rules_r, update)) {
+        if (check_order_1(rules, update)) {
             // println("{}", update[update.size() / 2]);
             res += update[update.size() / 2];
         } else {
             fix_order(rules, update);
+
             res2 += update[update.size() / 2];
         }
     }
