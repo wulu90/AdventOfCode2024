c4e63cbd727be6a5316dcba760dabe48a12f40cf
wulu <wulu90@outlook.com>
2024-12-09 15:34:18 +0800

    09 p2

--- 09.cpp
+++ 09.cpp
@@ -1,13 +1,10 @@
-#include <cstddef>
-#include <cstdint>
-#include <cstdlib>
 #include <fstream>
 #include <print>
 #include <vector>
 
 using namespace std;
 
-struct ff {
+struct layout {
     int fileid;
     int length;
 };
@@ -15,69 +12,145 @@ struct ff {
 void part1() {
     ifstream input("input/input-9");
 
-    vector<ff> ffvec;
-
-    int id         = 0;
+    vector<layout> diskmap;
+    string line;
+    getline(input, line);
     bool freespace = false;
-    while (!input.eof()) {
-        int len = input.get() - '0';
-        if (len == -38 || len == -49)
-            break;
+    int fileid         = 0;
+
+    for (auto c : line) {
+        int len = c - '0';
         if (len == 0) {
             freespace = false;
             continue;
         }
+
         if (!freespace) {
-            ffvec.push_back({id, len});
-            ++id;
+            diskmap.push_back({fileid, len});
+            ++fileid;
             freespace = true;
         } else {
-            ffvec.push_back({-1, len});
+            diskmap.push_back({-1, len});
             freespace = false;
         }
     }
 
     size_t i = 0;
-    size_t j = ffvec.size() - 1;
+    size_t j = diskmap.size() - 1;
 
-    vector<ff> re;
+    vector<layout> disknew;
     while (i < j) {
-        if (ffvec[i].fileid != -1) {
-            re.push_back(ffvec[i]);
+        if (diskmap[i].fileid != -1) {
+            disknew.push_back(diskmap[i]);
             ++i;
             continue;
         }
 
-        if (ffvec[j].fileid == -1) {
+        if (diskmap[j].fileid == -1) {
             --j;
             continue;
         }
 
-        if (ffvec[i].length > ffvec[j].length) {    // freespace > tail file length
-            re.push_back(ffvec[j]);
-            ffvec[i].length -= ffvec[j].length;
+        if (diskmap[i].length > diskmap[j].length) {    // freespace > tail file length
+            disknew.push_back(diskmap[j]);
+            diskmap[i].length -= diskmap[j].length;
             --j;
-        } else if (ffvec[i].length < ffvec[j].length) {
-            re.push_back({ffvec[j].fileid, ffvec[i].length});
-            ffvec[j].length -= ffvec[i].length;
+        } else if (diskmap[i].length < diskmap[j].length) {
+            disknew.push_back({diskmap[j].fileid, diskmap[i].length});
+            diskmap[j].length -= diskmap[i].length;
             ++i;
         } else {
-            re.push_back(ffvec[j]);
+            disknew.push_back(diskmap[j]);
             ++i;
             --j;
         }
     }
 
-    if(ffvec[j].fileid!=-1&&ffvec[j].length>0)
-        re.push_back(ffvec[j]);
+    if (diskmap[j].fileid != -1 && diskmap[j].length > 0)
+        disknew.push_back(diskmap[j]);
 
     int64_t res = 0;
-    id          = 0;
-    for (auto [fileid, len] : re) {
+    int blockid          = 0;
+    for (auto [fileid, len] : disknew) {
         while (len > 0) {
-            res += fileid * id;
-            ++id;
+            res += fileid * blockid;
+            ++blockid;
             --len;
+        }
+    }
+
+    println("{}", res);
+}
+
+void part2() {
+    ifstream input("input/input-9");
+
+    vector<layout> diskmap;
+    string line;
+    getline(input, line);
+    bool freespace = false;
+    int id         = 0;
+
+    for (auto c : line) {
+        int len = c - '0';
+        if (len == 0) {
+            freespace = false;
+            continue;
+        }
+
+        if (!freespace) {
+            diskmap.push_back({id, len});
+            ++id;
+            freespace = true;
+        } else {
+            diskmap.push_back({-1, len});
+            freespace = false;
+        }
+    }
+
+    // auto disknew = diskmap;
+
+    auto j = diskmap.size() - 1;
+
+    while (j != 0) {
+        if (diskmap[j].fileid == -1) {
+            --j;
+            continue;
+        }
+
+        bool find_enough_space = false;
+        int needsize           = diskmap[j].length;
+        size_t i               = 0;
+        for (i = 0; i < j; ++i) {
+            if (diskmap[i].fileid == -1 && diskmap[i].length >= needsize) {
+                find_enough_space = true;
+                break;
+            }
+        }
+
+        if (!find_enough_space) {
+            --j;
+            continue;
+        }
+
+        if (diskmap[i].length == needsize) {
+            diskmap[i].fileid = diskmap[j].fileid;
+            diskmap[j].fileid = -1;
+            --j;
+        } else {
+            diskmap[i].length -= needsize;
+            auto tmp          = diskmap[j];
+            diskmap[j].fileid = -1;
+            diskmap.insert(diskmap.cbegin() + i, tmp);
+        }
+    }
+    int64_t res = 0;
+    int blockid = 0;
+    for (auto [fileid, len] : diskmap) {
+        while (len > 0) {
+            res += blockid * (fileid == -1 ? 0 : fileid);
+            --len;
+            ++blockid;
         }
     }
 
@@ -86,5 +159,6 @@ void part1() {
 
 int main(int argc, char* argv[]) {
     part1();
+    part2();
     return 0;
 }
