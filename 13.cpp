db799915b19364483ee103d6cbeb10122c0e65d2
wulu <wulu90@outlook.com>
2024-12-13 14:58:18 +0800

    13 p2

--- 13.cpp
+++ 13.cpp
@@ -34,7 +34,6 @@ void parse_prize(const string& line, int& xp, int& yp) {
 
 void part1() {
     ifstream input("input/input-13");
-    // ifstream input("input-test");
     vector<claw> clawvec;
     for (string line; getline(input, line);) {
         if (!line.empty()) {
@@ -66,7 +65,54 @@ void part1() {
     println("{}", res);
 }
 
+void part2() {
+    ifstream input("input/input-13");
+    vector<claw> clawvec;
+    for (string line; getline(input, line);) {
+        if (!line.empty()) {
+            claw cl;
+            parse_xy(line, cl.xa, cl.ya);
+            getline(input, line);
+            parse_xy(line, cl.xb, cl.yb);
+            getline(input, line);
+            parse_prize(line, cl.xprize, cl.yprize);
+            clawvec.push_back(cl);
+        }
+    }
+    input.close();
+
+    /**
+     * xa*a+xb*b=xp; ya*a+yb*b=yp; a,b
+     * ①*ya ②*xa
+     * xa*ya*a+xb*ya*b=xp*ya
+     * xa*ya*a+yb*xa*b=yp*xa
+     * b=(yp*xa-xp*ya)/(yb*xa-xb*ya)
+     * ①*yb ②*xb
+     * xa*yb*a+xb*yb*b=xp*yb
+     * ya*xb*a+xb*yb*b=yp*xb
+     * a=(xp*yb-yp*xb)/(xa*yb-ya*xb)
+     **/
+
+    int64_t off{10000000000000};
+    int64_t res = 0;
+    for (auto cl : clawvec) {
+        int64_t divisor = cl.xa * cl.yb - cl.xb * cl.ya;
+        if (divisor == 0) {
+            continue;
+        }
+        int64_t a_dividend = (cl.xprize + off) * cl.yb - (cl.yprize + off) * cl.xb;
+        int64_t b_dividend = (cl.yprize + off) * cl.xa - (cl.xprize + off) * cl.ya;
+
+        if (a_dividend % divisor == 0 && b_dividend % divisor == 0) {
+            res += 3 * a_dividend / divisor + b_dividend / divisor;
+        }
+    }
+
+    println("{}", res);
+}
+
 int main(int argc, char* argv[]) {
     part1();
+    part2();
     return 0;
 }
