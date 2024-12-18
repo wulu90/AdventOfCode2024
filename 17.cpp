a22fa07b8ee2d3411f5a709988a4699436238db6
wulu <wulu90@outlook.com>
2024-12-18 12:44:25 +0800

    17 p2, cuda code is right, but 10 hours no result, mayby need optimization or 4090?🙃

--- 17.cpp
+++ 17.cpp
@@ -1,4 +1,5 @@
 #include <array>
+#include <cerrno>
 #include <charconv>
 #include <fmt/base.h>
 #include <fmt/core.h>
@@ -11,30 +12,9 @@
 #include <vector>
 using namespace std;
 
-void part1() {
-    ifstream input("input/input-17");
-    string line;
-    array<int, 3> regs;
-    for (size_t i = 0; i < 3; ++i) {
-        getline(input, line);
-        auto colonpos = line.find(':');
-        from_chars(line.data() + colonpos + 2, line.data() + line.length(), regs[i]);
-    }
-    getline(input, line);    // empty line;
-    getline(input, line);
-
-    auto colonpos        = line.find(':');
-    vector<int> programs = string_view{line.begin() + colonpos + 2, line.end()} | views::split(',') |
-                           views::transform([](auto subr) -> int {
-                               string_view subsv(subr.begin(), subr.end());
-                               int num = 0;
-                               from_chars(subsv.data(), subsv.data() + subsv.size(), num);
-                               return num;
-                           }) |
-                           ranges::to<vector>();
-
+vector<size_t> run(const vector<size_t>& programs, array<size_t, 3> regs) {
     size_t instrction_pointer = 0;
-    vector<int> outputs;
+    vector<size_t> outputs;
     while (true) {
         if (instrction_pointer >= programs.size()) {
             break;
@@ -71,10 +51,99 @@ void part1() {
         instrction_pointer += 2;
     }
 
+    return outputs;
+}
+
+pair<vector<size_t>, array<size_t, 3>> parse_input() {
+    ifstream input("input/input-17");
+    string line;
+    array<size_t, 3> regs;
+    for (size_t i = 0; i < 3; ++i) {
+        getline(input, line);
+        auto colonpos = line.find(':');
+        from_chars(line.data() + colonpos + 2, line.data() + line.length(), regs[i]);
+    }
+    getline(input, line);    // empty line;
+    getline(input, line);
+
+    auto colonpos           = line.find(':');
+    vector<size_t> programs = string_view{line.begin() + colonpos + 2, line.end()} | views::split(',') |
+                              views::transform([](auto subr) -> size_t {
+                                  string_view subsv(subr.begin(), subr.end());
+                                  int num = 0;
+                                  from_chars(subsv.data(), subsv.data() + subsv.size(), num);
+                                  return num;
+                              }) |
+                              ranges::to<vector>();
+
+    input.close();
+    return {programs, regs};
+}
+
+void part1() {
+    auto [programs, regs] = parse_input();
+    fmt::println("{}", fmt::join(run(programs, regs), ","));
+}
+
+vector<size_t> run2() {
+    vector<size_t> outputs;
+    size_t r0 = 136904920099226;
+    size_t r1 = 0;
+    size_t r2 = 0;
+
+    while (true) {
+        r1 = r0 % 8;
+        r1 ^= 5;
+        // r2 = r0 / (1ul << r1);
+        r2 = r0 >> r1;
+        r1 ^= 6;
+        // r0 /= 8;
+        r0 >>= 3;
+        r1 ^= r2;
+        outputs.push_back(r1 % 8);
+        if (r0 == 0)
+            break;
+    }
     fmt::println("{}", fmt::join(outputs, ","));
+    return outputs;
+}
+
+size_t run3(size_t r0) {
+    size_t r1 = r0 % 8;
+    r1 ^= 5;
+    size_t r2 = r0 >> r1;
+    r1 ^= 6;
+    r0 >>= 3;
+    r1 ^= r2;
+    return r1 % 8;
+}
+
+void dfs(size_t index, const vector<size_t>& programs, vector<size_t>& nums, size_t curr) {
+    for (size_t i = 0; i < 8; ++i) {
+        if (run3(curr * 8 + i) == programs[15 - index]) {
+            if (index == 15) {
+                nums.push_back(curr * 8 + i);
+                return;
+            }
+            dfs(index + 1, programs, nums, curr * 8 + i);
+        }
+    }
+}
+
+void part2() {
+    auto [programs, orig] = parse_input();
+    // vector<size_t> programs{2,4,1,5,7,5,1,6,0,3,4,6,5,5,3,0};
+    vector<size_t> nums;
+    dfs(0, programs, nums, 0);
+
+    for (auto n : nums) {
+        println("{}", n);
+    }
 }
 
 int main(int argc, char* argv[]) {
     part1();
+    run2();
+    part2();
     return 0;
 }
---
+++ 17.cu
@@ -0,0 +1,57 @@
+#include <cuda_runtime.h>
+#include <iostream>
+#include <limits>
+
+__constant__ __device__ int prog[16] = {2, 4, 1, 5, 7, 5, 1, 6, 0, 3, 4, 6, 5, 5, 3, 0};
+
+__global__ void check_num_kernel(unsigned long long n, unsigned long long* result) {
+    unsigned long long r0 = n + blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned long long r1 = 0;
+    unsigned long long r2 = 0;
+    int step              = 0;
+
+    while (step < 16) {
+        r1 = r0 % 8;
+        r1 ^= 5;
+        r2 = r0 >> r1;
+        r1 ^= 6;
+        r0 >>= 3;
+        r1 ^= r2;
+        if (r1 % 8 != prog[step]) {
+            break;
+        }
+        if (r0 == 0)
+            break;
+
+        ++step;
+    }
+
+    if (step == 15) {
+        *result = n + blockIdx.x * blockDim.x + threadIdx.x;
+    }
+}
+
+int main() {
+    // char* h_results = (char*)malloc(1024 * 32);
+    unsigned long long* h_result = new unsigned long long(0);
+    unsigned long long* d_result;
+    cudaMalloc(&d_result, sizeof(unsigned long long));
+
+    for (size_t i = 35184372088832; i < std::numeric_limits<size_t>::max(); i += 1024 * 32) {
+        // for (size_t i = 0; i < 1024 * 32 * 10; i += 1024 * 32) {
+        cudaMemset(d_result, 0, sizeof(unsigned long long));
+        check_num_kernel<<<32, 1024>>>(i, d_result);
+        cudaDeviceSynchronize();
+        cudaMemcpy(h_result, d_result, sizeof(unsigned long long), cudaMemcpyDeviceToHost);
+        if (*h_result != 0) {
+            std::cout << *h_result << std::endl;
+            break;
+        }
+    }
+    delete h_result;
+    cudaFree(d_result);
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess)
+        std::cout << "CUDA error: " << cudaGetErrorString(err) << std::endl;
+    // cudaProfilerStop();
+}
