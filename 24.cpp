5cf657c4f591799f5b44e99daae5bc596fbc71fb
wulu <wulu90@outlook.com>
2024-12-25 00:14:12 +0800

    24 p2 Christmas Eve 🎶

--- 24.cpp
+++ 24.cpp
@@ -1,4 +1,4 @@
-#include <cstdint>
+#include <algorithm>
 #include <fstream>
 #include <functional>
 #include <map>
@@ -11,23 +11,34 @@
 
 using namespace std;
 
+enum class gate { AND, OR, XOR };
+
 struct gate_wires {
     string wire_input_0;
     string wire_input_1;
     string wire_output;
-    function<uint8_t(uint8_t, uint8_t)> gate;
+    gate gate;
 };
 
-function<uint8_t(uint8_t, uint8_t)> parse_gate(const string_view& str) {
-    function<uint8_t(uint8_t, uint8_t)> func;
-    if (str == "AND") {
-        func = bit_and<uint8_t>();
+gate parse_gate(const string_view& str) {
+    if (str == "AND"sv) {
+        return gate::AND;
     } else if (str == "OR") {
-        func = bit_or<>();
-    } else if (str == "XOR") {
-        func = bit_xor<>();
+        return gate::OR;
+    } else {
+        return gate::XOR;
     }
-    return func;
+}
+
+function<uint8_t(uint8_t, uint8_t)> gatefunc(const gate& g) {
+    switch (g) {
+    case gate::AND:
+        return bit_and<uint8_t>();
+    case gate::OR:
+        return bit_or<uint8_t>();
+    default:
+        return bit_xor<uint8_t>();
+    }
 }
 
 void part1() {
@@ -48,7 +59,7 @@ void part1() {
         gw.wire_input_0 = string_view{*it};
         gw.gate         = parse_gate(string_view{*++it});
         gw.wire_input_1 = string_view{*++it};
-        ++it;
+        ++it;    // ->
         gw.wire_output = string_view{*++it};
         gate_wires_vec.push_back(gw);
         unknowset.insert(gw.wire_input_0);
@@ -64,7 +75,7 @@ void part1() {
     while (!unknowset.empty()) {
         for (const auto& gw : gate_wires_vec) {
             if (inputmap.contains(gw.wire_input_0) && inputmap.contains(gw.wire_input_1) && !inputmap.contains(gw.wire_output)) {
-                inputmap[gw.wire_output] = gw.gate(inputmap[gw.wire_input_0], inputmap[gw.wire_input_1]);
+                inputmap[gw.wire_output] = gatefunc(gw.gate)(inputmap[gw.wire_input_0], inputmap[gw.wire_input_1]);
                 unknowset.erase(gw.wire_output);
             }
         }
@@ -79,7 +90,114 @@ void part1() {
     println("{}", res);
 }
 
+string find_output(const string& x, const string& y, const gate& gate, const vector<gate_wires>& gate_wires_vec) {
+    string str;
+    for (const auto& gw : gate_wires_vec) {
+        if (gw.wire_input_0 == x && gw.wire_input_1 == y && gw.gate == gate) {
+            str = gw.wire_output;
+        }
+
+        if (gw.wire_input_0 == y && gw.wire_input_1 == x && gw.gate == gate) {
+            str = gw.wire_output;
+        }
+    }
+    return str;
+}
+
+pair<string, string> full_adder(const string& x, const string& y, const string& c_in, const vector<gate_wires>& gate_wires_vec,
+                                vector<string>& swapoutputs) {
+    // z: x XOR y XOR c_in
+    // c_out: (x AND y) OR ((x XOR y) AND c_in)
+    // m = x XOR y
+    // n = x AND y
+    // z = m XOR c_in
+    // r = m AND c_in
+    // c_out = n OR r
+
+    auto m = find_output(x, y, gate::XOR, gate_wires_vec);
+    auto n = find_output(x, y, gate::AND, gate_wires_vec);
+
+    if (!c_in.empty()) {
+        auto r = find_output(m, c_in, gate::AND, gate_wires_vec);
+        if (r.empty()) {
+            swap(m, n);
+            swapoutputs.push_back(m);
+            swapoutputs.push_back(n);
+            r = find_output(m, c_in, gate::AND, gate_wires_vec);
+        }
+
+        auto z = find_output(m, c_in, gate::XOR, gate_wires_vec);
+        if (m.starts_with('z')) {
+            swap(m, z);
+            swapoutputs.push_back(m);
+            swapoutputs.push_back(z);
+        }
+
+        if (n.starts_with('z')) {
+            swap(n, z);
+            swapoutputs.push_back(z);
+            swapoutputs.push_back(n);
+        }
+
+        if (r.starts_with('z')) {
+            swap(r, z);
+            swapoutputs.push_back(r);
+            swapoutputs.push_back(z);
+        }
+
+        auto c_out = find_output(n, r, gate::OR, gate_wires_vec);
+        if (c_out.starts_with('z') && c_out != "z45"s) {    // last carry out
+            swap(c_out, z);
+            swapoutputs.push_back(c_out);
+            swapoutputs.push_back(z);
+        }
+        if (c_out.empty()) {    // ?
+            c_out = m;
+        }
+
+        return {z, c_out};
+    } else {
+        return {m, n};
+    }
+}
+
+void part2() {
+    ifstream input{"input/input-24"};
+    for (string line; getline(input, line) && !line.empty();) {}
+
+    vector<gate_wires> gate_wires_vec;
+    for (string line; getline(input, line);) {
+        gate_wires gw;
+
+        auto splitview  = line | ranges::views::split(" "sv);
+        auto it         = splitview.cbegin();
+        gw.wire_input_0 = string_view{*it};
+        gw.gate         = parse_gate(string_view{*++it});
+        gw.wire_input_1 = string_view{*++it};
+        ++it;    // ->
+        gw.wire_output = string_view{*++it};
+        gate_wires_vec.push_back(gw);
+    }
+    input.close();
+
+    // https://en.wikipedia.org/wiki/Adder_(electronics)#Full_adder
+    // https://github.com/xhyrom/aoc/blob/main/2024/24/part_2.py
+    vector<string> swapoutputs;
+    string x, y, c;
+    for (int i = 0; i < 45; ++i) {
+        x = format("x{:02}", i);
+        y = format("y{:02}", i);
+
+        auto [z, c_out] = full_adder(x, y, c, gate_wires_vec, swapoutputs);
+        c               = c_out;
+    }
+    sort(swapoutputs.begin(), swapoutputs.end());
+    for_each_n(swapoutputs.begin(), swapoutputs.size() - 1, [](const auto& s) { return print("{},", s); });
+    println("{}", *swapoutputs.crbegin());
+}
+
 int main(int argc, char* argv[]) {
     part1();
+    part2();
     return 0;
 }
